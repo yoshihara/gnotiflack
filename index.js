d5424879edd47da67322ee98d89dd45c1db810b1
yoshihara <h.yoshihara@everyleaf.com>
2016-11-03 20:30:10 +0900

    Use `request` module for GitHub request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const https = require('https');
+const request = require('request');
 
 console.log('Loading function');
 
@@ -8,68 +9,53 @@ exports.handler = (event, context, callback) => {
     var token = event.token;
     var body = "";
 
-    var github = https.request(
-        {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json",
-                "User-Agent": "Awesome-Octocat-App",
-                "Authorization": `token ${event.token}`
-            },
-            host: "api.github.com",
-            path: "/notifications"
-        },
-
-        (githubResponse) =>{
-            githubResponse.setEncoding('utf8');
-            githubResponse.on('data', (chunk) => {
-
-                console.log(`github BODY: ${chunk}`);
-                body = body + chunk;
-            });
-
-            githubResponse.on('end', () => {
-                JSON.parse(body).forEach((notification) => {
-                    var data = JSON.stringify({
-                        "attachments": [
-                            {
-                                "title": notification.subject.title,
-                                "text": notification.subject.url.replace(/api\./, '').replace(/repos\//, '')
-                            }
-                        ]
-                    });
-
-                    var slack = https.request(
-                        {
-                            method: "POST",
-                            headers: {
-                                "Content-type": "application/json"
-                            },
-                            host: "hooks.slack.com",
-                            path: event.path
-                        },
-                        (response) =>{
-                            response.setEncoding('utf8');
-                            response.on('data', (chunk) => {
-                                console.log(`BODY: ${chunk}`);
-                            });
-                            response.on('end', () => {
-                                console.log('No more data in response.');
-                            });
-                        }
-                    );
-
-                    slack.on('error', (e) => {
-                        console.log(`problem with request: ${e.message}`);
-                    });
-
-                    slack.write(data);
-                    slack.end();
-                });
-            });
+    var githubOptions = {
+        url: "https://api.github.com/notifications",
+        headers: {
+            "Content-type": "application/json",
+            "User-Agent": "Awesome-Octocat-App",
+            "Authorization": `token ${event.token}`
         }
-    );
+    }
 
-    github.end();
+    request(githubOptions, (error, response, body) => {
+        JSON.parse(body).forEach((notification) => {
+            var data = JSON.stringify({
+                "attachments": [
+                    {
+                        "title": notification.subject.title,
+                        "text": notification.subject.url.replace(/api\./, '').replace(/repos\//, '')
+                    }
+                ]
+            });
+
+            var slack = https.request(
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    host: "hooks.slack.com",
+                    path: event.path
+                },
+                (response) =>{
+                    response.setEncoding('utf8');
+                    response.on('data', (chunk) => {
+                        console.log(`BODY: ${chunk}`);
+                    });
+                    response.on('end', () => {
+                        console.log('No more data in response.');
+                    });
+                }
+            );
+
+            slack.on('error', (e) => {
+                console.log(`problem with request: ${e.message}`);
+            });
+
+            slack.write(data);
+            slack.end();
+        });
+    });
     callback(null, 'hoge');  // Echo back the first key value
 };
