398561da4e0714562c5d8a3d42c0ca91f3a95db4
yoshihara <h.yoshihara@everyleaf.com>
2016-11-03 20:39:38 +0900

    Use single quote instead of double quote

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
     var token = event.token;
-    var body = "";
+    var body = '';
 
     var githubOptions = {
-        url: "https://api.github.com/notifications",
+        url: 'https://api.github.com/notifications',
         headers: {
-            "Content-type": "application/json",
-            "User-Agent": "Awesome-Octocat-App",
-            "Authorization": `token ${event.token}`
+            'Content-type': 'application/json',
+            'User-Agent': 'Awesome-Octocat-App',
+            'Authorization': `token ${event.token}`
         }
     }
 
     var slackOptions = {
         url: `https://hooks.slack.com${event.path}`,
         headers: {
-            "Content-type": "application/json"
+            'Content-type': 'application/json'
         }
     };
 
     request(githubOptions, (error, response, body) => {
         JSON.parse(body).forEach((notification) => {
             var data = JSON.stringify({
-                "attachments": [
+                'attachments': [
                     {
-                        "title": notification.subject.title,
-                        "text": notification.subject.url.replace(/api\./, '').replace(/repos\//, '')
+                        'title': notification.subject.title,
+                        'text': notification.subject.url.replace(/api\./, '').replace(/repos\//, '')
                     }
                 ]
             });
